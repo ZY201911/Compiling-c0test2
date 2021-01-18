547f6a7e9471d7d20777ff12fdf727e5e3c4f7b9
Yang <1341967103@qq.com>
2021-01-18 21:50:24 +0800

    v0.1

--- App.java
+++ App.java
@@ -10,7 +10,7 @@ public class App {
             StringIter it = new StringIter(scanner);
             Tokenizer tokenizer = new Tokenizer(it);
             Analyser analyser = new Analyser(tokenizer);
-//            throw new Exception("异常")；
+            throw new Exception("异常");
         }
         catch (Exception e){
             System.exit(-1);
